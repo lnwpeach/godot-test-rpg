f47d268eb15b122a726627eb11bb29295de4d111
peach <pichayuth.c@gmail.com>
2020-06-25 22:12:41 +0700

    update bat wander state

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -21,6 +21,10 @@ onready var stats = $Stats
 onready var playerDectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
+
+func _ready():
+	state = pick_random_state([IDLE, WANDER])
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -30,24 +34,46 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+			
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+				
+			accelerate_towards_point(wanderController.target_position, delta)
+			
+			if global_position.distance_to(wanderController.target_position) <= 4:
+				update_wander()
+			
 		CHASE:
 			var player = playerDectionZone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				accelerate_towards_point(player.global_position, delta)
 			else:
 				state = IDLE
-			sprite.flip_h = velocity.x < 0
 			
 	if softCollision.is_colliding():
 		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
 
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1,3))
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocity.x < 0
+
 func seek_player():
 	if playerDectionZone.can_see_player():
 		state = CHASE
+		
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
